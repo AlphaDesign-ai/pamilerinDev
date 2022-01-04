c35d420bacb8c1964b8f6850420cd45d3c027cac
AlphaDesign-ai <holluwapamilerin1@gmail.com>
2022-01-04 04:33:06 +0100

    added support for mobile mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,17 @@
 import '../styles/globals.scss';
 import { AppProps } from 'next/app';
 import Navigation from '../components/Navigation';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className="app">
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        />
+      </Head>
       <Navigation />
       <Component {...pageProps} />
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
 import Contact from '../components/Contact';
 import Hero from '../components/Hero';
+import Head from 'next/head';
 
 const shortInfoAboutMe = `Hello, I am developer/designer passionate about web, based in Lagos, Nigeria`;
 
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Pamilerin</title>
+      </Head>
       <Hero
         content={shortInfoAboutMe}
         className="main-hero"
