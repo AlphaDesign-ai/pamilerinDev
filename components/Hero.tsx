66474d759ae6e936c57c91ca615f6d528583748f
AlphaDesign-ai <holluwapamilerin1@gmail.com>
2022-02-08 16:19:47 +0100

    edit my content

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,21 +4,24 @@ const Hero: FC = () => {
   return (
     <div className="main-hero">
       <p>
-        Hello, I am Lawal Yusuf, A frontend developer/designer based in Lagos,
-        Nigeria. I&apos;m passionate about building web profuct that are
+        Hello, I am Lawal Yusuf, A Frontend developer / designer based in Lagos,
+        Nigeria. I&apos;m passionate about building web product that are
         accessible usable to everyone and taking accessibility at it core.
       </p>
       <p>
-        Also I am a Electrical and Electronic undergraduate, having interned
-        with about two start up over the past One year. &quot;HNG&quot; and
-        &quot;sidehustle&quot; internship.The latter which I just finihed last
-        december where I was able to work different person which boosted my team
-        spirit.
+        I am a Electrical and Electronic undergraduate, have interned with two
+        start up over the last One year. &quot;HNG&quot; and
+        &quot;Sidehustle&quot; internship program. During the course of the
+        internship, I was priviledge to have worked with intelligent people,
+        which has better my skills as a frontend developer in term of
+        communication, collaboration, team spirit.
       </p>
 
       <p>
-        I have being working professionally with Javascript for the past 2
-        years.
+        I been working with tools like Typescript, Javascript, React, Next js,
+        Redux in building highly scalable web product. Recently, i have been
+        exploring the design world and I think having a little bit to design
+        knownledge is good for a Frontend developer.
       </p>
     </div>
   );
