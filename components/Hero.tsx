2e4d4016cc1ae532dc606decee88b3c5249ab7ac
AlphaDesign-ai <holluwapamilerin1@gmail.com>
2022-02-08 04:53:36 +0100

    fix issue with css

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,14 +5,15 @@ const Hero: FC = () => {
     <div className="main-hero">
       <p>
         Hello, I am Lawal Yusuf, A frontend developer/designer based in Lagos,
-        Nigeria. I'm passionate about building web profuct that are accessible
-        usable to everyone and taking accessibility at it core.
+        Nigeria. I&apos;m passionate about building web profuct that are
+        accessible usable to everyone and taking accessibility at it core.
       </p>
       <p>
         Also I am a Electrical and Electronic undergraduate, having interned
-        with about two start up over the past One year. "HNG" and "sidehustle"
-        internship.The latter which I just finihed last december where I was
-        able to work different person which boosted my team spirit.
+        with about two start up over the past One year. &quot;HNG&quot; and
+        &quot;sidehustle&quot; internship.The latter which I just finihed last
+        december where I was able to work different person which boosted my team
+        spirit.
       </p>
 
       <p>
